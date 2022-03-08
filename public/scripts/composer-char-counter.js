d28979f899c2fc7f731ece1a146f9c2e770b75c1
Khadija Mohamed <khadijam@live.ca>
2022-03-08 01:53:05 -0500

    implementation of character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
+  $(".tweet-text").on('input', function() {
+    let charactersCounted = $(this).val().length;
+    let remainingCharacters = 140 - charactersCounted;
+    let counter = $(this).parent().next('div').children('.counter');
+    counter.text(remainingCharacters);
+  });
 })
