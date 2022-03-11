6e2674bb8a117739d13c5add0af7a2a853e19f3f
Khadija Mohamed <khadijam@live.ca>
2022-03-11 08:51:44 -0500

    updated deleted directory

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,59 +1,56 @@
+
 $(document).ready(function () {
 
-  const loadTweets = function() {
-    $.ajax({ method: 'GET', url: '/tweets'
-    }).then((response) => {
+  const loadTweets = function () {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+    })
+    .then((response) => {
       renderTweets(response);
       $('.tweet-text').val("");
       $(".counter").text(140);
     });
   };
   loadTweets();
-  
 
-  $('.tweetsSent').on('submit', function(event){
+  $('.new-tweet-form').on('submit', function (event) {
     event.preventDefault();
+    console.log('testing submission')
+
+    const createTweetElement = function (tweet) {
+      let time = timeago.format(tweet.created_at);
+      let $tweet = 
+      $(
+        `<article>
+          <header>
+            <div class="icon"><img src="/images/profile-hex.png"> </div>
+            <div class="name">Rosey</div>
+            <div class="username">@username</div>
+          </header>
+          <main class='tweet-content'>
+            <span>I have seen further by standing on the shoulder of giants</span>
+          </main>
+          <footer>
+            <div class="time">${time}</div>
+            <div class="buttons">
+              <span><i class="fa fa-flag" aria-hidden="true"></i></span>
+              <span><i class="fa fa-retweet" aria-hidden="true"></i></span>
+              <span><i class="fa fa-heart" aria-hidden="true"></i></span>
+            </div>
+        </section>`)
+        
+      return $tweet;
+    };
+
+    const renderTweets = function (tweets) {
+      $(".tweetsSent").empty();
+      console.log($('.tweets'));
+      for (let tweet of tweets) {
+        $('.tweets').prepend(createTweetElement(tweet));
+      }
+    }
+    renderTweets(data);
   })
+});  
 
-  let submission = $('.tweetsSent').serialize();
-  $.ajax({ method: 'GET', url: '/tweets', data:submission
-})
-.then((response) => {
-  renderTweets(response);
-  $('.tweet-text').val("");
-  $(".counter").text(140);
-});
-loadTweets();
-
-const renderTweets = function(tweets) {
-
-  for (let tweet of tweets) {
-    $('.tweets').prepend(createTweetElement(tweet))}
-}
-
-const createTweetElement = function(tweet) {
-let time = timeago.format(tweet.created_at);
-let $tweet = $(`<section class="tweets">
-<article>
-  <header>
-    <div class="icon"><img src="/images/profile-hex.png"> </div>
-    <div class="name">Rosey</div>
-    <div class="username">@username</div>
-  </header>
-  <main class='tweet-content'>
-    <span>I have seen further by standing on the shoulder of giants</span>
-  </main>
-  <footer>
-    <div class="time">${time}</div>
-    <div class="buttons">
-      <span><i class="fa fa-flag" aria-hidden="true"></i></span>
-      <span><i class="fa fa-retweet" aria-hidden="true"></i></span>
-      <span><i class="fa fa-heart" aria-hidden="true"></i></span>
-    </div>
-</section>`)
-return $tweet;
-};
-
-renderTweets(data);
-
-}); 
