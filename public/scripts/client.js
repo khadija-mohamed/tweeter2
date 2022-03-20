59c15953596ae7d3a4db8e12aa46ba4b68f02dce
Khadija Mohamed <khadijam@live.ca>
2022-03-20 04:42:13 -0400

    creation of mock html with template strings

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -24,15 +24,33 @@ const data = [
   }
 ]
 
-const renderTweets = function(tweets) {
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const value = createTweetElement(tweet);
+    $('.tweets').prepend(value)
+  }
 }
 
+// creation of mock HTML with javascript template strings 
 const createTweetElement = function(tweet) {
-let $tweet = /* Your code for creating the tweet element */
-// ...
+let $tweet = `
+<article id="tweet">
+<header class="tweet-header">
+  <img src="${tweet.user.avatars}">
+    <h2 class="username">${tweet.user.name}</h2>
+    <span class="handle">${tweet.user.handle}</span>
+</header>
+<section>
+  <p>${tweet.content.text}</p>
+  </section>
+  <footer class="tweet-footer">
+  <p2>${tweet.content.created_at}</p2>
+</footer>
+</article>
+`
 return $tweet;
 }
 
